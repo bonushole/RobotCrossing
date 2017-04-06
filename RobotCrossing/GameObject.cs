302c0ad1a2b7ca741870238d6b578e373b6be105
bonushole <156max@gmail.com>
2017-04-05 23:03:16 -0400

    added game object class

---
+++ RobotCrossing/GameObject.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotCrossing
+{
+    class GameObject
+    {
+        Vector2 position;
+        Texture2D texture;
+        
+        public GameObject(Texture2D texture, Vector2 position)
+        {
+            this.position = position;
+            this.texture = texture;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position);
+        }
+    }
+}
