80eab7ed4e356fa2f4ac122d59d7d0716c4710e7
bonushole <156max@gmail.com>
2017-04-05 23:20:05 -0400

    added object spawning

--- RobotCrossing/GameScene.cs
+++ RobotCrossing/GameScene.cs
@@ -11,8 +11,18 @@ namespace RobotCrossing
     class GameScene : Scene
     {
         Player player;
+        double lastspawn = 0;
+        double spawnperiod=5;
+        Random rnd = new Random();
+        List<GameObject> objects = new List<GameObject>();
         public override void Update(GameTime gameTime){
             player.Update(gameTime);
+
+            if (gameTime.TotalGameTime.TotalSeconds >= (lastspawn+spawnperiod))
+            {
+                objects.Add(new GameObject(TextureManager.getTexture2D("cursor"), new Vector2(rnd.Next(100), rnd.Next(100))));
+                lastspawn = gameTime.TotalGameTime.TotalSeconds;
+            }
         }
         public override void LoadContent(GameWindow window){
             player = new Player(window);
@@ -24,6 +34,10 @@ namespace RobotCrossing
             spriteBatch.Begin(transformMatrix:
               Matrix.CreateTranslation(
                   new Vector3((player.position.X * -1) + window.ClientBounds.Width / 2, (player.position.Y * -1) + window.ClientBounds.Height / 2, 0)));
+            foreach (GameObject obj in objects)
+            {
+                obj.Draw(spriteBatch);
+            }
             player.Draw(spriteBatch);
             spriteBatch.End();
         }
